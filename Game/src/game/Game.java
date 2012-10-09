81ed6abc4c5e33f1dd23b21e859b305d33535efc
pcgames3112 <>
2012-10-09 22:41:41 +0200

    lwjgl

--- Game/src/game/Game.java
+++ Game/src/game/Game.java
@@ -1,13 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package game;
-
-/**
- *
- * @author Andi
- */
 public class Game {
 
     /**
