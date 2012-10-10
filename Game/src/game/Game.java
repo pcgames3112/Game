7f86e60b5cdcd025ae9943d09370379b6d7eb31d
jonas <post@jonas-burger.de>
2012-10-10 17:26:16 +0200

    first window

--- Game/src/game/Game.java
+++ Game/src/game/Game.java
@@ -1,10 +1,42 @@
 package game;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class Game {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Tree lololol!");
+        System.out.println("OK, lets go");
+        
+        Game game = new Game();
+        game.start();
+        
     }
+    
+    protected void start() {
+    	try {
+			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+    	
+    	// init OPGL here
+    	
+    	while(!Display.isCloseRequested()) {
+    		
+    		// render stuff here/
+    		
+    		Display.update();
+    	}
+    	
+    	Display.destroy();
+    }
+    
+    
 }
