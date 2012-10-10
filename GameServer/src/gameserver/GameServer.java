5a644d0d8fa83ce9abfe514036631c61576fc561
pcgames3112 <>
2012-10-10 17:13:44 +0200

    Copied Game to GameServer

---
+++ GameServer/src/gameserver/GameServer.java
@@ -0,0 +1,10 @@
+package gameserver;
+public class GameServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Tree lololol!");
+    }
+}
